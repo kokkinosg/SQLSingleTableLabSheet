0d45c6b09b9411cd5a373a316e7dd5caf08db7b5
Georgios Kokkinos <gk769@bath.ac.uk>
2025-05-23 17:06:01 +0100

    Answered all Q1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SQLSingleTableLabSheet
 An example repository to help set up codespaces for SQLite for use with the Cloud and Databases Online course at the University of Bath
+
+ # What I learned
+ Clicking Run Querry runs everyting on the .sql file
+Highlight a querry and click Run Selected Querry 
--- main.sql
+++ main.sql
@@ -1,7 +1,46 @@
 
 -- Single Table Lab Sheet
 
-CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups SMALLINT UNSIGNED, birth DATE, death DATE);
 
-. schema
-. table
+CREATE TABLE pet 
+(name VARCHAR(20), 
+owner VARCHAR(20), 
+species VARCHAR(20), 
+sex CHAR(1), 
+checkups SMALLINT, 
+birth DATE, 
+death DATE);
+
+
+INSERT INTO pet (name,owner,species,sex,checkups,birth,death)VALUES
+('Fluffy','Harold','cat','f',5,'2001-02-04',NULL),
+('Claws','Gwen','cat','m',2,'2000-03-17',NULL),
+('Buffy','Harold','dog','f',7,'1999-05-13',NULL),
+('Fang','Benny','dog','m',4,'2000-08-27',NULL),
+('Bowser','Diane','dog','m',8,'1998-08-31','2001-07-29'),
+('Chirpy','Gwen','bird','f',0,'2002-09-11',NULL),
+('Whistler','Gwen','bird','',1,'2001-12-09',NULL),
+('Slim','Benny','snake','M',5,'2001-04-29',NULL);
+
+SELECT * FROM pet;
+
+SELECT * FROM pet WHERE sex = "m";
+
+SELECT owner FROM pet WHERE sex = "m";
+
+SELECT name, species, sex FROM pet WHERE species = "snake" OR species
+= "bird";
+
+SELECT owner, name FROM pet WHERE sex = "f";
+
+SELECT name, birth FROM pet WHERE species = "dog";
+
+SELECT owner FROM pet WHERE species = "bird";
+
+SELECT species FROM pet WHERE sex ="f";
+
+SELECT name, birth FROM pet WHERE species = "cat" OR species = "dog";
+
+SELECT name, species FROM pet WHERE species = "cat" OR species = "bird" AND sex ="f";
+
+SELECT name FROM pet WHERE sex < "m";
