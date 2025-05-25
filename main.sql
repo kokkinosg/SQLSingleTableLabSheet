262438f6e7e5a45e7c46412fa806eb14652b9d98
Georgios Kokkinos <gk769@bath.ac.uk>
2025-05-25 10:50:48 +0100

    Answered all Q2

--- main.sql
+++ main.sql
@@ -22,6 +22,8 @@ INSERT INTO pet (name,owner,species,sex,checkups,birth,death)VALUES
 ('Whistler','Gwen','bird','',1,'2001-12-09',NULL),
 ('Slim','Benny','snake','M',5,'2001-04-29',NULL);
 
+-- Q1
+
 SELECT * FROM pet;
 
 SELECT * FROM pet WHERE sex = "m";
@@ -43,4 +45,22 @@ SELECT name, birth FROM pet WHERE species = "cat" OR species = "dog";
 
 SELECT name, species FROM pet WHERE species = "cat" OR species = "bird" AND sex ="f";
 
+-- Q2 
+
 SELECT name FROM pet WHERE sex < "m";
+
+SELECT owner, name FROM pet WHERE name LIKE "%er" OR name LIKE "%all";
+
+SELECT name FROM pet WHERE name LIKE "%e%";
+
+SELECT name FROM pet WHERE name NOT LIKE "%fy";
+
+SELECT name FROM pet WHERE LENGTH(name) = 4;
+
+-- Substring is extract 1 character from string, starting at position 1 (second attribute)
+
+SELECT owner FROM pet WHERE SUBSTR(owner,1,1) IN ('a','b', 'c', 'd', 'e') AND 
+SUBSTR(owner,-1,1) IN ('a','b', 'c', 'd', 'e');
+
+SELECT owner FROM pet WHERE UPPER(SUBSTR(owner,1,1)) IN ('A','B', 'C', 'D', 'E') AND 
+LOWER(SUBSTR(owner,-1,1)) IN ('a','b', 'c', 'd', 'e');
