80b7b8c7127abcbd59d940a4bffe51edbd4334bc
Georgios Kokkinos <gk769@bath.ac.uk>
2025-05-25 22:05:59 +0100

    Answered All Q3

--- main.sql
+++ main.sql
@@ -20,7 +20,7 @@ INSERT INTO pet (name,owner,species,sex,checkups,birth,death)VALUES
 ('Bowser','Diane','dog','m',8,'1998-08-31','2001-07-29'),
 ('Chirpy','Gwen','bird','f',0,'2002-09-11',NULL),
 ('Whistler','Gwen','bird','',1,'2001-12-09',NULL),
-('Slim','Benny','snake','M',5,'2001-04-29',NULL);
+('Slim','Benny','snake','m',5,'2001-04-29',NULL);
 
 -- Q1
 
@@ -64,3 +64,34 @@ SUBSTR(owner,-1,1) IN ('a','b', 'c', 'd', 'e');
 
 SELECT owner FROM pet WHERE UPPER(SUBSTR(owner,1,1)) IN ('A','B', 'C', 'D', 'E') AND 
 LOWER(SUBSTR(owner,-1,1)) IN ('a','b', 'c', 'd', 'e');
+
+
+-- Q3
+SELECT name, strftime('%m', birth)AS Month FROM pet WHERE death IS NOT NULL;
+
+SELECT owner, name, birth, MIN(strftime('%Y',birth)) AS birth FROM pet
+GROUP BY owner;
+
+SELECT owner, AVG(checkups) AS Average_Checkups FROM pet GROUP BY owner;
+
+SELECT species, COUNT(species) AS total_number FROM pet GROUP BY species ORDER BY total_number ASC;
+
+--  The number of pets of each species that each owner has
+
+SELECT owner, species, COUNT(species) AS total_num_species FROM pet GROUP BY owner,species;
+
+-- Q3-4. The number of distinct species of pet each owner has
+
+SELECT owner, COUNT(DISTINCT species) AS num_distinc_species FROM pet GROUP BY owner;
+
+-- The number of pets of each gender there are in the database, where the gender is known
+
+SELECT sex, COUNT(sex) FROM pet WHERE sex IS NOT NULL AND sex != '' GROUP BY sex;
+
+-- The number of birds each owner has
+
+SELECT owner, SUM(species = 'bird') AS 'number of birds' FROM pet GROUP BY owner;
+
+--  The total number of check-ups each owner has made with all their pets
+
+SELECT owner, SUM(checkups) AS 'Number of checkups' FROM pet GROUP BY owner;
